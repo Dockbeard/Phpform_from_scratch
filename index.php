e2b74ff8a22ce2c8f1705af7966e36f9c1c91e5a
Dockbeard <nicolas.galas@espacece.fr>
2021-05-09 21:16:01 +0200

    new button

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php ob_start(); ?>
 
-<a href="http://localhost/Phpform_from_scratch/view/registration.php"><button class="btn btn-outline-success my-2 my-sm-0 " type="submit">Connexion</button></a>
+<a href="http://localhost/Phpform_from_scratch/view/connection.php"><button class="btn btn-outline-success my-2 my-sm-0 " type="submit">Connexion</button></a>
+<a href="http://localhost/Phpform_from_scratch/view/registration.php"><button class="btn btn-primary " type="submit">S'inscrire</button></a>
 <?php $content = ob_get_clean();
 require('./templates/template.php'); ?>
